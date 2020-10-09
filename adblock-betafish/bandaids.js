5d6370d6de60885e7303d75e2cdf2e4344881d86
BrentM <brent.montrose@gmail.com>
2020-10-09 15:49:57 -0600

    Updated with version: 4.23.0 changes

--- adblock-betafish/bandaids.js
+++ adblock-betafish/bandaids.js
@@ -222,6 +222,89 @@ const runBandaids = function () {
           };
         }
       }
+
+      // add click handler for adblock subscribe clicks
+      // similiar to the code here:
+      // https://github.com/adblockplus/adblockpluschrome/blob/master/subscriptionLink.postload.js
+      // the link host check ('subscribe.getadblock.com') is specific to the getadblock.com domain
+      document.addEventListener('click', (event) => {
+        // Ignore right-clicks
+        if (event.button === 2) {
+          return;
+        }
+
+        // Ignore simulated clicks.
+        if (event.isTrusted === false) {
+          return;
+        }
+
+        // Search the link associated with the click
+        let link = event.target;
+        while (!(link instanceof HTMLAnchorElement)) {
+          link = link.parentNode;
+
+          if (!link) {
+            return;
+          }
+        }
+
+        let queryString = null;
+        if (link.protocol === 'http:' || link.protocol === 'https:') {
+          if (link.host === 'subscribe.getadblock.com' && link.pathname === '/') {
+            queryString = link.search.substr(1);
+          }
+        } else {
+          return;
+        }
+
+        if (!queryString) {
+          return;
+        }
+
+        // This is our link - make sure the browser doesn't handle it
+        event.preventDefault();
+        event.stopPropagation();
+
+        // Decode URL parameters
+        let title = null;
+        let url = null;
+        for (const param of queryString.split('&')) {
+          const parts = param.split('=', 2);
+          if (parts.length === 2) {
+            switch (parts[0]) {
+              case 'title':
+                title = decodeURIComponent(parts[1]);
+                break;
+              case 'location':
+                url = decodeURIComponent(parts[1]);
+                break;
+              default: // do nothing
+            }
+          }
+        }
+        if (!url) {
+          return;
+        }
+
+        // Default title to the URL
+        if (!title) {
+          title = url;
+        }
+
+        // Trim spaces in title and URL
+        title = title.trim();
+        url = url.trim();
+        if (!/^(https?|ftp):/.test(url)) {
+          return;
+        }
+
+        browser.runtime.sendMessage({
+          type: 'subscriptions.add',
+          title,
+          url,
+          confirm: true,
+        });
+      }, true);
     },
   }; // end bandaids
 
